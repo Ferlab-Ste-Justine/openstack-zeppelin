185bd29d66a925d46349be277bf0a2709ddfbadf
Issam <ihannache@ferlab.bio>
2024-03-19 14:41:57 -0400

    feat: INFRA-403 Added mising cloud config variables

--- main.tf
+++ main.tf
@@ -7,35 +7,43 @@ locals {
     delete_on_termination = false
   }] : []
   cloudinit_templates = concat([
-      {
-        filename     = "zeppelin.cfg"
-        content_type = "text/cloud-config"
-        content      = templatefile(
-          "${path.module}/templates/cloud_config.yaml",
-          {
-            nameserver_ips          = var.nameserver_ips
-            zeppelin_version        = var.zeppelin_version
-            zeppelin_mirror         = var.zeppelin_mirror
-            k8_executor_image       = var.k8_executor_image
-            k8_api_endpoint         = var.k8_api_endpoint
-            s3_access               = var.s3_access
-            s3_secret               = var.s3_secret
-            s3_url                  = var.s3_url
-            hive_metastore_url      = var.hive_metastore_url
-            spark_sql_warehouse_dir = var.spark_sql_warehouse_dir
-            k8_ca_certificate       = var.k8_ca_certificate
-            k8_client_certificate   = var.k8_client_certificate
-            k8_client_private_key   = var.k8_client_private_key
-            notebook_s3_bucket      = var.notebook_s3_bucket
-            keycloak                = var.keycloak
-          }
-        )
-      },
-      {
-        filename     = "node_exporter.cfg"
-        content_type = "text/cloud-config"
-        content      = module.prometheus_node_exporter_configs.configuration
-      }
+    {
+      filename     = "zeppelin.cfg"
+      content_type = "text/cloud-config"
+      content = templatefile(
+        "${path.module}/templates/cloud_config.yaml",
+        {
+          nameserver_ips                   = var.nameserver_ips
+          zeppelin_version                 = var.zeppelin_version
+          zeppelin_mirror                  = var.zeppelin_mirror
+          k8_executor_image                = var.k8_executor_image
+          k8_api_endpoint                  = var.k8_api_endpoint
+          s3_access                        = var.s3_access
+          s3_secret                        = var.s3_secret
+          s3_url                           = var.s3_url
+          hive_metastore_url               = var.hive_metastore_url
+          spark_sql_warehouse_dir          = var.spark_sql_warehouse_dir
+          k8_ca_certificate                = var.k8_ca_certificate
+          k8_client_certificate            = var.k8_client_certificate
+          k8_client_private_key            = var.k8_client_private_key
+          notebook_s3_bucket               = var.notebook_s3_bucket
+          keycloak                         = var.keycloak
+          k8_service_account_name          = var.k8_service_account_name
+          k8_namespace                     = var.k8_namespace
+          k8_secret_s3                     = var.k8_secret_s3
+          k8_secret_s3_access_key          = var.k8_secret_s3_access_key
+          k8_secret_s3_secret_key          = var.k8_secret_s3_secret_key
+          spark_dynamic_allocation_enabled = var.spark_dynamic_allocation_enabled
+          spark_max_executors              = var.spark_max_executors
+          spark_min_executors              = var.spark_min_executors
+        }
+      )
+    },
+    {
+      filename     = "node_exporter.cfg"
+      content_type = "text/cloud-config"
+      content      = module.prometheus_node_exporter_configs.configuration
+    }
     ],
     var.fluentbit.enabled ? [{
       filename     = "fluent_bit.cfg"
@@ -54,7 +62,7 @@ module "fluentbit_configs" {
   source               = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//fluent-bit?ref=v0.14.2"
   install_dependencies = true
   fluentbit = {
-    metrics          = var.fluentbit.metrics
+    metrics = var.fluentbit.metrics
     systemd_services = [
       {
         tag     = var.fluentbit.zeppelin_tag
@@ -70,7 +78,7 @@ module "fluentbit_configs" {
 }
 
 data "template_cloudinit_config" "zeppelin" {
-  gzip = true
+  gzip          = true
   base64_encode = true
   dynamic "part" {
     for_each = local.cloudinit_templates
@@ -83,8 +91,8 @@ data "template_cloudinit_config" "zeppelin" {
 }
 
 resource "openstack_networking_port_v2" "zeppelin" {
-  name           = var.name
-  network_id     = var.network_id
+  name       = var.name
+  network_id = var.network_id
   security_group_ids = concat(
     var.additional_security_group_ids,
     [openstack_networking_secgroup_v2.zeppelin_server.id]
@@ -93,11 +101,11 @@ resource "openstack_networking_port_v2" "zeppelin" {
 }
 
 resource "openstack_compute_instance_v2" "zeppelin" {
-  name            = var.name
-  image_id        = var.image_source.image_id != "" ? var.image_source.image_id : null
-  flavor_id       = var.flavor_id
-  key_pair        = var.keypair_name
-  user_data       = data.template_cloudinit_config.zeppelin.rendered
+  name      = var.name
+  image_id  = var.image_source.image_id != "" ? var.image_source.image_id : null
+  flavor_id = var.flavor_id
+  key_pair  = var.keypair_name
+  user_data = data.template_cloudinit_config.zeppelin.rendered
 
   network {
     port = openstack_networking_port_v2.zeppelin.id
--- variables.tf
+++ variables.tf
@@ -1,193 +1,199 @@
 variable "name" {
   description = "Name to give to the vm, its port and the prefix of security groups"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "image_source" {
   description = "Source of the vm's image"
   type = object({
-    image_id = string
+    image_id  = string
     volume_id = string
   })
 }
 
 variable "flavor_id" {
   description = "ID of the VM flavor for the node"
-  type = string
+  type        = string
 }
 
 variable "kubernetes_workers_security_group_id" {
   description = "Id of the security group for the kubernetes workers zeppelin will interact with"
-  type = string
+  type        = string
 }
 
 variable "additional_security_group_ids" {
   description = "Additional security groups of the node"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "fluentd_security_group" {
   description = "Fluentd security group configuration"
   type = object({
-    id = string
+    id   = string
     port = number
   })
   default = {
-    id = ""
+    id   = ""
     port = 0
   }
 }
 
 variable "network_id" {
   description = "Id of the network the node will be attached to"
-  type = string
+  type        = string
 }
 
 variable "keypair_name" {
   description = "Name of the keypair that will be used to ssh to the node"
-  type = string
+  type        = string
 }
 
 variable "client_group_ids" {
   description = "Id of client security groups"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "bastion_group_ids" {
   description = "Id of bastion security groups"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "metrics_server_group_ids" {
   description = "Id of metric servers security groups"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "nameserver_ips" {
   description = "Ips of the nameservers"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "zeppelin_version" {
   description = "Version of zeppelin"
-  type = string
-  default = "0.11.0"
+  type        = string
+  default     = "0.11.0"
 }
 
 variable "zeppelin_mirror" {
   description = "Mirror from which to download zeppelin"
-  type = string
-  default = "https://mirror.csclub.uwaterloo.ca/apache"
+  type        = string
+  default     = "https://mirror.csclub.uwaterloo.ca/apache"
 }
 
 variable "k8_executor_image" {
   description = "Image to launch k8 executor from"
-  type = string
-  default = "apache/spark:3.5.1"
+  type        = string
+  default     = "apache/spark:3.5.1"
 }
 
 variable "k8_service_account_name" {
   description = "Service account name to use"
-  type = string
+  type        = string
 }
 
 variable "k8_namespace" {
   description = "Namespace to use"
-  type = string
+  type        = string
 }
 
 variable "k8_api_endpoint" {
   description = "Endpoint to access the k8 masters"
-  type = string
+  type        = string
 }
 
 variable "k8_ca_certificate" {
   description = "CA certicate of kubernetes api"
-  type = string
+  type        = string
 }
 
 variable "k8_client_certificate" {
   description = "Client certicate to access kubernetes api"
-  type = string
+  type        = string
 }
 
 variable "k8_secret_s3" {
   description = "Name of kubernetes secret to use for S3 credentials for executors pods"
-  type = string
+  type        = string
 }
 
 variable "k8_secret_s3_access_key" {
   description = "Key in k8_secret_s3 secret which contains the S3 access key"
-  type = string
+  type        = string
 }
 
 variable "k8_secret_s3_secret_key" {
   description = "Key in k8_secret_s3 secret which contains the S3 secret key"
-  type = string
+  type        = string
 }
+
+variable "k8_client_private_key" {
+  description = "Client private key to access kubernetes api"
+  type        = string
+}
+
 
 variable "s3_access" {
   description = "S3 access key"
-  type = string
+  type        = string
 }
 
 variable "s3_secret" {
   description = "S3 secret key"
-  type = string
+  type        = string
 }
 
 variable "s3_url" {
   description = "url of the S3 store"
-  type = string
+  type        = string
 }
 
 variable "hive_metastore_port" {
   description = "Port of the hive metastore on the kubernetes cluster"
-  type = number
+  type        = number
 }
 
 variable "hive_metastore_url" {
   description = "Url of the hive metastore"
-  type = string
+  type        = string
 }
 
 variable "spark_sql_warehouse_dir" {
   description = "S3 path of the spark sql warehouse"
-  type = string
+  type        = string
 }
 
 variable "spark_max_executors" {
   description = "Number maximum of spark executors"
-  type = number
-  default = 15
+  type        = number
+  default     = 15
 }
 
 variable "spark_min_executors" {
   description = "Number minimum of spark executors"
-  type = number
-  default = 0
+  type        = number
+  default     = 0
 }
 
-variable spark_dynamic_allocation_enabled {
+variable "spark_dynamic_allocation_enabled" {
   description = "Enable dynamic allocation of spark executors"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "notebook_s3_bucket" {
   description = "S3 bucket to store notebooks under"
-  type = string
+  type        = string
 }
 
 variable "keycloak" {
   description = "Keycloak configuration for user authentication"
-  type        = object({
+  type = object({
     enabled       = bool
     url           = string
     realm         = string
@@ -211,7 +217,7 @@ variable "fluentbit" {
     enabled           = bool
     zeppelin_tag      = string
     node_exporter_tag = string
-    metrics           = object({
+    metrics = object({
       enabled = bool
       port    = number
     })
@@ -227,7 +233,7 @@ variable "fluentbit" {
     enabled           = false
     zeppelin_tag      = ""
     node_exporter_tag = ""
-    metrics           = {
+    metrics = {
       enabled = false
       port    = 0
     }
